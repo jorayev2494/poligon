15a90dd3effce52a169ff6c766832c6dc85ced97
“Yakub <yjorayev@gmail.com>
2021-08-01 13:29:40 +0300

    redis connected

--- config/database.php
+++ config/database.php
@@ -91,6 +91,33 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'redis' => [
+            'client' => env('REDIS_CLIENT', 'predis'),
+
+            'options' => [
+                'cluster' => env('REDIS_CLUSTER', 'redis'),
+                'prefix' => env('REDIS_PREFIX'),
+            ],
+
+            // for Default
+            'default' => [
+                'url' => env('REDIS_URL'),
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', '6379'),
+                'database' => env('REDIS_DATABASE', '0')
+            ],
+
+            // for Cache
+            'cache' => [
+                'url' => env('REDIS_URL'),
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', '6379'),
+                'database' => env('REDIS_DATABASE', '1')
+            ]
+        ]
+
     ],
 
     /*
